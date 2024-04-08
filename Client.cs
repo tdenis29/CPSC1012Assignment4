47fe303d6aa2b86aa69111a46505a170f1dc65bf
tdenis29 <76631691+tdenis29@users.noreply.github.com>
2024-04-08 12:49:05 -0600

    inital commit

---
+++ Client.cs
@@ -0,0 +1,21 @@
+// The program build will be developed in two parts:
+// Part A – Class and Object Implementation
+// Client Class
+// Design a class named Client that meets the following requirements:
+//     A string field to store the client's first name and a corresponding property FirstName with both get and set
+//         The first name cannot be empty, null, or whitespace; ensure that the stored value is trimmed of leading and trailing whitespace
+//     A string field to store the client's last name and a corresponding property LastName with both get and set
+//         The last name cannot be empty, null, or whitespace; ensure that the stored value is trimmed of leading and trailing whitespace
+//     An int field to store the client's weight in pounds and a corresponding property Weight with both get and set
+//         The weight must be greater than zero
+//     An int field to store the client's height in inches and a corresponding property Height with both get and set
+//         The weight must be greater than zero
+//     A greedy constructor that requires the first name, last name, weight, and height as parameters
+//         Use the properties in the constructor for setting the fields to take advantage of any validation checks already coded
+//     A read-only property named BmiScore that will return as a double the BMI score for the client
+//     A read-only property named BmiStatus that will return as a string the BMI status for the corresponding BMI score
+//     A read-only property named FullName that will return as a string the client's full name in the format Lastname, FirstName
+namespace ClientClass;
+class Client(){
+    
+}
